3773d6fff6fea8b7f946a7ca53ad54a491278b0b
FF66887 <76743748+FF66887@users.noreply.github.com>
2025-07-11 02:40:53 +0800

    custom

--- gollum/custom.js
+++ gollum/custom.js
@@ -1,15 +1,19 @@
 // Google Analytics (gtag.js)
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+// Load Google Analytics script and configure when it's ready
 (function() {
   var script = document.createElement('script');
   script.async = true;
   script.src = 'https://www.googletagmanager.com/gtag/js?id=G-ZCWWBBG7BH';
+  script.onload = function() {
+    // Configure Google Analytics after script loads
+    gtag('config', 'G-ZCWWBBG7BH', {
+      'anonymize_ip': true,
+      'cookie_expires': 63072000  // 2 years
+    });
+  };
   document.head.appendChild(script);
 })();
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-ZCWWBBG7BH', {
-  'anonymize_ip': true,
-  'cookie_expires': 63072000  // 2 years
-});
