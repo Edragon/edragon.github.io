0efedadd52994def3212dae55fa100eac2a88c74
FF66887 <76743748+FF66887@users.noreply.github.com>
2025-07-20 19:28:56 +0800

    config

--- gollum/config.rb
+++ gollum/config.rb
@@ -14,13 +14,20 @@ wiki_options = {
   # Basic settings
   h1_title: true,
   allow_editing: false,
-  hyphened_tag_lookup: true,
-  case_insensitive_tag_lookup: true,
-  global_tag_lookup: true,
-  pagination_count: 5,
   template_dir: '/root/edragon.github.io/gollum'
-
-
 }
 
 Precious::App.set(:wiki_options, wiki_options)
+
+# Increase worker processes for better performance
+Gollum::Hook.register(:post_commit, :hook_id) do |committer, sha1|
+  # Add any post-commit hooks here
+end
+
+# Configure Gollum options
+options = {
+  :port => 4567,
+  :bind => '127.0.0.1',
+  :threads => 4,
+  :workers => 2
+}
